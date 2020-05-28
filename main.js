823107c8fc8958b5d8586003b2b7ffe9101cf655
Bruno Anthony Shimura <bruno_shimura@hotmail.com>
2020-05-28 13:35:08 -0300

    solução básica

--- main.js
+++ main.js
@@ -36,7 +36,7 @@ function criarForm(variaveis, restricoes) {
         i +
         '" name="y' +
         i +
-        '" type="number" value="" id="example-number-input"></div><label for="example-number-input" class="col-form-label">x' +
+        '" type="float" value="" id="example-number-input"></div><label for="example-number-input" class="col-form-label">x' +
         i +
         mais +
         "</label>";
@@ -53,7 +53,7 @@ function criarForm(variaveis, restricoes) {
           '" name="x' +
           i +
           j +
-          '" type="number" value="" id="example-number-input"> </div> <label for="example-number-input" class="col-form-label">x' +
+          '" type="float" value="" id="example-number-input"> </div> <label for="example-number-input" class="col-form-label">x' +
           j +
           mais +
           "</label>";
@@ -63,14 +63,14 @@ function criarForm(variaveis, restricoes) {
         i +
         '" name="b' +
         i +
-        '" type="number" value="" id="example-number-input"></div></div>';
+        '" type="float" value="" id="example-number-input"></div></div>';
     }
     txt +=
       '<h1></h1><div class="card bg-light text-dark"> <div class="card-body"> <h4 class="card-title">Informe os dados iniciais:</h4> <form action="" method="post" name="form2" id="form2"> <div class="form-group"> <div class="form-group "> <h6>Função objetivo:</h6> <div class="form-group row"> ' +
       funcao +
       " </div>" +
       restricao +
-      '<button type="button" onClick="criarMatriz(variaveis.value,regras.value)" class="btn btn-primary">Continuar</button> <button onClick="atualizar()" type="reset" class="btn btn-secondary">Novo</button> <img onClick="manual()" type="submit" id="help" class="img-responsive" src="img/help.png" width="30" height="30"/>  </form> </div> </div> </div> ';
+      '<button id="btn2" type="button" onClick="criarMatriz(variaveis.value,regras.value)" class="btn btn-primary">Continuar</button> <button onClick="atualizar()" type="reset" class="btn btn-secondary" id="novo1">Novo</button> <img onClick="manual()" type="submit" id="help1" class="img-responsive" src="img/help.png" width="30" height="30"/>  </form> </div> </div> </div> ';
     document.getElementById("form1").innerHTML = txt;
     document.getElementById("btn1").style.display = "none";
     document.getElementById("help").style.display = "none";
@@ -84,31 +84,30 @@ function criarForm(variaveis, restricoes) {
 // cria tabela
 //==============================================================================
 function criarTable(variaveis, restricoes, matriz) {
-  if (validaMatriz(variaveis, restricoes)) {
-  } else {
-    var titulo = "";
-    var txt = "";
-    var corpo = "";
-    var total = parseInt(variaveis) + parseInt(restricoes);
-    for (var i = 0; i <= total; i++) {
-      titulo += '<th scope="col">' + matriz[0][i] + "</th>";
-    }
-    for (var i = 1; i <= restricoes; i++) {
-      
-      corpo += '<tr><th scope="row">' + matriz[i][0] + "</th>";
-      for (var j = 1; j <= total; j++) {
-        corpo += "<td>" + matriz[i][j] + "</td>";
-      }
-      corpo += "</tr>";
-    }
-    txt =
-      '<h1></h1> <div class="card bg-light text-dark"> <div class="card-body"> <h4 class="card-title">Solução básica:</h4> <form action="/action_page.php"> <div class="form-group"> <table class="table table-striped"> <thead> <tr> ' +
-      titulo +
-      "  </tr> </thead> <tbody> " +
-      corpo +
-      '</tbody> </table> </div> <button type="submit" class="btn btn-primary">Continuar</button> </form> </div> </div> </div>';
-    document.getElementById("table1").innerHTML = txt;
+  var titulo = "";
+  var txt = "";
+  var corpo = "";
+  var total = 1 + parseInt(variaveis) + parseInt(restricoes);
+  for (var i = 0; i <= total; i++) {
+    titulo += '<th scope="col">' + matriz[0][i] + "</th>";
   }
+  for (var i = 0; i <= restricoes; i++) {
+    corpo += '<tr><th scope="row">' + matriz[i + 1][0] + "</th>";
+    for (var j = 1; j <= total; j++) {
+      corpo += "<td>" + matriz[i + 1][j] + "</td>";
+    }
+    corpo += "</tr>";
+  }
+  txt =
+    '<h1></h1> <div class="card bg-light text-dark"> <div class="card-body"> <h4 class="card-title">Solução básica:</h4> <form action="/action_page.php"> <div class="form-group"> <table class="table table-striped"> <thead> <tr> ' +
+    titulo +
+    "  </tr> </thead> <tbody> " +
+    corpo +
+    '</tbody> </table> </div> <button id="btn3" type="button" onClick="criarMatriz(variaveis.value,regras.value)" class="btn btn-primary">Continuar</button> <button onClick="atualizar()" type="reset" class="btn btn-secondary" id="novo2">Novo</button> <img onClick="manual()" type="submit" id="help2" class="img-responsive" src="img/help.png" width="30" height="30"/> </form> </div> </div> </div>';
+  document.getElementById("table1").innerHTML = txt;
+  document.getElementById("btn2").style.display = "none";
+  document.getElementById("help1").style.display = "none";
+  document.getElementById("novo1").style.display = "none";
 }
 //==============================================================================
 // cria tabela
@@ -120,14 +119,14 @@ function atualizar() {
 // valida valores
 //==============================================================================
 
-function validaMatriz(variaveis, restricoes) {
-  for (i = 1; i <= variaveis; i++) {
+function validarCoeficientes(p_variaveis, p_restricoes) {
+  for (i = 1; i <= p_variaveis; i++) {
     if (document.getElementById("y" + i).value == "") {
       document.getElementById("y" + i).focus();
       alert("Informe os valores de todos os coeficientes.");
       return 1;
     }
-    for (j = 1; j <= restricoes; j++) {
+    for (j = 1; j <= p_restricoes; j++) {
       if (document.getElementById("x" + j + i).value == "") {
         document.getElementById("x" + j + i).focus();
         alert("Informe os valores de todos os coeficientes.");
@@ -135,7 +134,7 @@ function validaMatriz(variaveis, restricoes) {
       }
     }
   }
-  for (j = 1; j <= variaveis; j++) {
+  for (j = 1; j <= p_restricoes; j++) {
     if (document.getElementById("b" + j).value == "") {
       document.getElementById("b" + j).focus();
       alert("Informe os valores de todas as constantes.");
@@ -149,8 +148,68 @@ function validaMatriz(variaveis, restricoes) {
 
 function criarMatriz(variaveis, restricoes) {
   //indices da matriz
-  var matriz = [[]];
-  matriz[0][] = "Base";
-  //============================================================================
+  if (validarCoeficientes(variaveis, restricoes) == 1) {
+    return;
+  }
+  esconder(variaveis, restricoes);
+  var total = 1 + parseInt(variaveis) + parseInt(restricoes);
+  var matriz = new Array();
+  matriz[0] = ["Base"];
+  var indice = 1;
+  for (var l = 1; l <= variaveis; l++) {
+    matriz[0][indice] = "x" + indice;
+    indice++;
+  }
+  for (var m = 1; m <= restricoes; m++) {
+    matriz[0][indice] = "f" + m;
+    indice++;
+  }
+
+  matriz[0][matriz[0].length] = "b";
+
+  for (var i = 1; i <= restricoes; i++) {
+    matriz[i] = ["f" + i];
+    if (i == restricoes) matriz[i + 1] = ["Lucro"];
+  }
+
+  for (i = 1; i <= variaveis; i++) {
+    matriz[parseInt(restricoes) + 1][i] =
+      -1 * parseFloat(document.getElementById("y" + i).value.replace(",", "."));
+    for (j = 1; j <= restricoes; j++) {
+      matriz[j][i] = parseFloat(
+        document.getElementById("x" + j + i).value.replace(",", ".")
+      );
+    }
+  }
+  for (j = 1; j <= restricoes; j++) {
+    matriz[j][total] = parseFloat(
+      document.getElementById("b" + j).value.replace(",", ".")
+    );
+  }
+  for (i = 1; i <= parseInt(restricoes) + 1; i++) {
+    for (j = 1; j <= total; j++) {
+      if (matriz[i][0] == matriz[0][j]) matriz[i][j] = 1;
+      if (matriz[i][j] == null) matriz[i][j] = 0;
+    }
+  }
+  alert(matriz);
   criarTable(variaveis, restricoes, matriz);
+}
+
+function esconder(p_variaveis, p_restricoes) {
+  for (i = 1; i <= p_variaveis; i++) {
+    document.getElementById("y" + i).style = "-moz-appearance:textfield;";
+    document.getElementById("y" + i).style.border = "0";
+    document.getElementById("y" + i).readOnly = true;
+    for (j = 1; j <= p_restricoes; j++) {
+      document.getElementById("x" + j + i).style = "-moz-appearance:textfield;";
+      document.getElementById("x" + j + i).style.border = "0";
+      document.getElementById("x" + j + i).readOnly = true;
+    }
+  }
+  for (j = 1; j <= p_restricoes; j++) {
+    document.getElementById("b" + j).style = "-moz-appearance:textfield;";
+    document.getElementById("b" + j).style.border = "0";
+    document.getElementById("b" + j).readOnly = true;
+  }
 }
